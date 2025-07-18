05d8b503b1a6c0ba276b7766ae95aa2cd356d02e
Richard Hristov <richardhristov@gmail.com>
2025-07-18 18:46:47 +0300

    fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
   },
 });
